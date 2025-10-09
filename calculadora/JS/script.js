d1595a6bef81a3e46a85b31b1925c93234286395
igor-adriano <igor.santos555@etec.sp.gov.br>
2025-10-09 17:00:21 -0300

    Add files via upload

---
+++ calculadora/JS/script.js
@@ -0,0 +1,22 @@
+const clique = () => {
+    let numero1 = parseFloat(document.getElementById("N1").value)
+
+    let numero2 = parseFloat(document.getElementById("N2").value)
+
+    if (isNaN(numero1) && isNaN(numero2)){
+        alert("Alguma coluna não esá preenchida")
+    }else{
+        alert("Calculando...")
+        let total = numero1+numero2;
+
+        exibir(total);
+    }
+
+}
+
+const exibir = (total) => {
+
+    document.getElementById("total").innerHTML = total;
+}
+
+
